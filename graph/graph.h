8e5578a8444035168b0241c3dd836a7b3fb97690
Saurav Chittal <chittal3@illinois.edu>
2021-11-28 18:43:49 -0500

    Rough graph.h draft

--- graph/graph.h
+++ graph/graph.h
@@ -1,1 +1,54 @@
 #pragma once
+
+#include <iostream>
+#include <vector>
+
+class Graph {
+
+	struct LinkedList {
+		int value;
+		LinkedList *next;
+
+		LinkedList(int value) {
+			this->value = value;
+			this->next = nullptr;
+		}
+
+		LinkedList(int value, LinkedList *next) {
+			this->value = value;
+			this->next = next;
+		}
+	};
+
+	struct AdjacencyList {
+		LinkedList *head;
+		int num_edges;
+
+		AdjacencyList() {
+			this->head = nullptr;
+			this->num_edges = 0;
+		}
+
+		AdjacencyList(LinkedList *head) {
+			this->head = head;
+			this->num_edges = 0;
+		}
+	};
+
+	public:
+		Graph(int num_vertices);
+		Graph();
+
+		void add_edge(int source, int destination);
+		void remove_edge(int source, int destination);
+		void print_graph();
+
+		bool isAdjacent(int source, int destination);
+
+		AdjacencyList adjacent(int vertex);
+
+	private:
+		std::vector<AdjacencyList> adjacency_list;
+		int num_vertices;
+};
+		
