e84432803e65c81d277cecc88a1ce63a00ed40dc
zpzhang2 <zpzhang2@illinois.edu>
2021-11-29 18:49:19 -0600

    add basic BFS method declarations

--- bfs/bfs.h
+++ bfs/bfs.h
@@ -1,11 +1,14 @@
 #pragma once
 
 #include <queue>
+#include <vector>
+#include "../graph/graph.h"
 
 class BFS: {
 	public:
-		
+		BFS(Graph * graph);
+		std::vector<int> findPath(int start, int end);
 	
 	private:
-		
+		Graph * graph_;
 };
