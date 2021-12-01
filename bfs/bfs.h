ed5d4ac27f76ee8a0393c4a9c7c554405b636815
zpzhang2 <zpzhang2@illinois.edu>
2021-12-01 14:20:28 -0600

    add isSC() to bfs.h

--- bfs/bfs.h
+++ bfs/bfs.h
@@ -7,7 +7,8 @@
 class BFS {
 	public:
 		BFS(Graph * graph);
-		std::vector<int> findPath(int start, int end);
+		std::vector<int> findPath(int start, int end); //returns empty vector if no path found
+		bool isSC(int start); //find if graph is strongly connected, from starting point
 	
 	private:
 		Graph * graph_;
