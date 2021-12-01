aba49601965cbf75ea0131aa03023fc779241e17
Maaheen Maajed <mmaajed2@linux-a3.ews.illinois.edu>
2021-11-30 22:05:17 -0600

    graph implementation: add, remove, isAdjacent, and some helpers in graph.h

--- graph/graph.cpp
+++ graph/graph.cpp
@@ -19,3 +19,109 @@ void Graph::Graph print_graph() {
         std::cout << std::endl;
     }
 }
+
+
+
+/*
+iterate through adajency_list and look at heads
+
+if source not head of any:
+  make new adjlist
+  place dest into adjlist
+  insert into vec
+if dest not head of any:
+  make new adjlist
+  place source into adjlist
+  insert into vec
+*/
+void Graph::insert_into_adjlist(unsigned list_idx, string to_insert) {
+    // adjacency_list[list_idx].push_back(to_insert);
+    // add to end of linked list
+    adjacency_list[list_idx].head->insert_at_end(to_insert);
+    adjacency_list[list_idx].num_edges += 1;
+}
+
+unsigned find_adjlist_idx(string to_find) {
+    for (unsigned i = 0; i < adjacency_list.size(); i++) {
+        if (adjacency_list[i].head->value == source) {
+            return i;
+        }
+    }
+
+    return adjacency_list.size(); // default value
+}
+
+void Graph::add_edge(string source, string destination) {
+    if (source == NULL || destination == NULL) {
+        std::cout << "NULL SOURCE OR DEST TO add_edge()" << std::endl;
+        return;
+    }
+
+    bool found_source = false;
+    unsigned source_idx = find_adjlist_idx(source);
+    unsigned dest_idx = find_adjlist_idx(destination);
+    unsigned start_size = adjacency_list.size(); // because this can change as we go
+
+    if (source_idx != start_size) {
+        insert_into_adjlist(source_idx, dest);
+    } else {
+        // make new adj node
+        AdjacencyList source_list = AdjacencyList();
+        source_list.insert_at_end(dest);
+        source_list.num_edges += 1;
+
+        adjacency_list.push_back(source_list);
+    }
+
+    if (dest_idx != start_size) {
+        insert_into_adjlist(source_idx, dest);
+    } else {
+        // make new adj node
+        AdjacencyList dest_list = AdjacencyList(dest_l_list);
+        dest_list.insert_at_end(source);
+        dest_list.num_edges += 1;
+
+        adjacency_list.push_back(dest_list);
+    }
+}
+
+void Graph::remove_edge(string source, string destination) {
+    if (source == NULL || destination == NULL) {
+        std::cout << "NULL SOURCE OR DEST TO remove_edge()" << std::endl;
+        return;
+    }
+
+    // first ensure that both source and dest actually exist in the adj list
+    unsigned start_idx = adjacency_list.size();
+    unsigned source_idx = find_adjlist_idx(source);
+    unsigned dest_idx = find_adjlist_idx(destination);
+
+    if (source_idx == start_idx || dest_idx == start_idx) {
+        std::cout << "ONE OR BOTH ITEMS DIDN'T EXIST IN remove_edge()" << std::endl;
+        return;
+    }
+
+    // remove them from each other's lists
+    // don't remove them from the adj list overall
+
+    adjacency_list[source_idx].remove(destination);
+    adjacency_list[dest_idx].remove(source);
+}
+
+bool Graph::isAdjacent(string source, string destination) {
+    unsigned source_idx = find_adjlist_idx(source);
+    unsigned dest_idx = find_adjlist_idx(destination);
+
+    if (source_idx == adjacency_list.size() || dest_idx == adjacency_list.size()) {
+        std::cout << "Not adjacent; one or both didn't exist!" << std::endl;
+        return false;
+    }
+
+    // watch out for issues if adjacency_list[dest_idx].find(source) == NULL or != NULL)
+    return (adjacency_list[source_idx].find(destination) != NULL);
+}
+
+
+Graph::AdjacencyList Graph::adjacent(string vertex) {
+    // 
+}
--- graph/graph.h
+++ graph/graph.h
@@ -6,18 +6,20 @@
 #include <fstream>
 #include <pair>
 
+using std::string;
+
 class Graph {
 
 	struct LinkedList {
-		int value;
+		string value;
 		LinkedList *next;
 
-		LinkedList(int value) {
+		LinkedList(string value) {
 			this->value = value;
 			this->next = nullptr;
 		}
 
-		LinkedList(int value, LinkedList *next) {
+		LinkedList(string value, LinkedList *next) {
 			this->value = value;
 			this->next = next;
 		}
@@ -36,24 +38,107 @@ class Graph {
 			this->head = head;
 			this->num_edges = 0;
 		}
+
+		bool remove_node(string to_remove) {
+			if (head->value == to_remove) {
+				// special case
+				LinkedList* temp = head;
+				head = head->next;
+				delete temp;
+				return;
+			}
+
+			LinkedList* prev = head;
+			LinkedList* current = head;
+			while (current != NULL) {
+				current = current->next;
+				if (current->value == to_remove) {
+					// remove this node
+					prev->next = current->next;
+					delete current;
+					return;
+				}
+				prev = prev->next;
+			}
+		}
+
+		void insert_at_end(string to_insert) {
+			if (head == NULL) {
+				head = new LinkedList(to_insert);
+				return;
+			}
+
+
+			LinkedList* current = head;
+			while (current->next != NULL) {
+				current = current->next;
+			}
+			current->next = LinkedList(to_insert);
+		}
+
+		// returns null if not found
+		LinkedList* find_prev(string to_find) { 
+			LinkedList* current = head;
+			while (current != NULL) {
+				if (current->next->value == to_find) {
+					return current;
+				}
+				current = current->next;
+			}
+			return NULL;
+		}
+
+		// // returns null if not found
+		// LinkedList* find(string to_find) {
+		// 	LinkedList* current = head;
+		// 	while (current != NULL) {
+		// 		if (current->value == to_find) {
+		// 			return current;
+		// 		}
+		// 		current = current->next;
+		// 	}
+		// 	return NULL;
+		// }
+
+		void remove(string to_remove) {
+			if (head == NULL) {
+				return;
+			}
+
+			if (head->value == to_remove) {
+				LinkedList* temp = head;
+				head = head->next;
+				delete head;
+			}
+
+			LinkedList* remove = find_prev(to_remove);
+			if (remove == NULL) {
+				return;
+			}
+			LinkedList* temp = remove->next;
+			remove->next = temp->next;
+			delete temp;
+		}
 	};
 
 	public:
 		Graph(int num_vertices);
 		Graph(std::string file1, std::string file2);
 
-		void add_edge(int source, int destination);
-		void remove_edge(int source, int destination);
+		void add_edge(string source, string destination);
+		void remove_edge(string source, string destination);
 		void print_graph();
 
-		bool isAdjacent(int source, int destination);
+		bool isAdjacent(string source, string destination);
 
-		AdjacencyList adjacent(int vertex);
+		AdjacencyList adjacent(string vertex);
 
 	private:
 		std::vector<AdjacencyList> adjacency_list;
 		int num_vertices;
 
 		std::vector<std::pair<int, int>> parserHelper(std::string file1, std::string file2);
+		void insert_into_adjlist(unsigned list_idx, string to_insert);
+		unsigned find_adjlist_idx(string to_find); // if this doesn't find it, it will return adjlist.size
+
 };
-		
