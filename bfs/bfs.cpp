6e687119ece597bf021576a61d09dd5e0bc7d868
zpzhang2 <zpzhang2@illinois.edu>
2021-12-08 14:36:51 -0600

    using vector from Graph::adjacent()

--- bfs/bfs.cpp
+++ bfs/bfs.cpp
@@ -6,30 +6,40 @@ BFS::BFS(Graph * graph) {
     graph_ = graph;
 }
 
-std::vector<string *> BFS::findPath(string * start, string * end) {
-    std::queue<string *> queue;
+std::vector<string> BFS::findPath(string & start, string & end) {
+    std::queue<string> queue;
     int num_vertices = graph_->get_num_vertices();
-    std::map<string *, int> distances;
-    std::map<string *, int> predecessors;
-    string * currentVtx;
+    std::map<string, int> distances;
+    std::map<string, int> predecessors;
+    string currentVtx;
 
     //starting point
     queue.push(start);
     distances[start] = 0;
 
+    bool found = false;
+
     //search until queue empty
     while (!queue.empty()) {
+        //break if found
+        if (queue.front() == end) {
+            break;
+        }
+
         currentVtx = queue.front();
         queue.pop();
 
-        // find incident edges, push into queue if not visited
-        // Graph::AdjacencyList adjacent = graph_->adjacent(*currentVtx);
-        
-
+        //find incident edges, push into queue if not visited
+        std::vector<string> adjacent = graph_->adjacent(currentVtx);
+        for (auto & vtx : adjacent) {
+            if (distances.find(vtx) != distances.end()) {
+                queue.push(vtx);
+            }
+        }
     }
     
     
-    std::vector<string *> output;
+    std::vector<string> output;
     
     return output;
 }
--- bfs/bfs.h
+++ bfs/bfs.h
@@ -8,7 +8,7 @@
 class BFS {
 	public:
 		BFS(Graph * graph);
-		std::vector<std::string *> findPath(std::string * start, std::string * end); //returns empty vector if no path found
+		std::vector<std::string> findPath(std::string & start, std::string & end); //returns empty vector if no path found
 		bool isSC(std::string * start); //find if graph is strongly connected, from starting point
 	
 	private:
