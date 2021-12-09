09c58b33f1216ce2fd5db69af1f6e5ff5fa40d2d
zpzhang2 <zpzhang2@illinois.edu>
2021-12-08 19:03:40 -0600

    add path finalizing for bfs, but it's probably broken

--- bfs/bfs.cpp
+++ bfs/bfs.cpp
@@ -10,10 +10,10 @@ std::vector<string> BFS::findPath(string & start, string & end) {
     std::queue<string> queue;
     int num_vertices = graph_->get_num_vertices();
     std::unordered_map<string, int> distances;
-    std::unordered_map<string, int> predecessors;
+    std::unordered_map<string, string> predecessors;
     string currentVtx;
 
-    //starting point
+    //starting point: distance is 0, no predecessor
     queue.push(start);
     distances[start] = 0;
 
@@ -23,6 +23,7 @@ std::vector<string> BFS::findPath(string & start, string & end) {
     while (!queue.empty()) {
         //break if found
         if (queue.front() == end) {
+            found = true;
             break;
         }
 
@@ -32,14 +33,24 @@ std::vector<string> BFS::findPath(string & start, string & end) {
         //find incident edges, push into queue if not visited
         std::vector<string> adjacent = graph_->adjacent(currentVtx);
         for (auto & vtx : adjacent) {
-            if (distances.find(vtx) != distances.end()) {
+            if (distances.find(vtx) != distances.end()) { //if not already found
                 queue.push(vtx);
+                predecessors[vtx] = currentVtx;
             }
         }
     }
     
-    
+    //return empty vector if not found
+    if (!found) {
+        return std::vector<string>();
+    }
+
+    //finalize output
     std::vector<string> output;
-    
-    return output;
+    output.push_back(end);
+    currentVtx = end;
+    while (predecessors.find(currentVtx) != predecessors.end()) {
+        output.push_back(predecessors.find(currentVtx));
+        currentVtx = predecessors.find(currentVtx);
+    }
 }
