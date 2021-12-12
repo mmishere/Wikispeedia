a14037faa1de1fccb881666586d0065083b60ad7
emicb <emicb55@gmail.com>
2021-12-12 20:15:09 +0000

    Added Centrality Saving Test

--- tests/util-tests.cpp
+++ tests/util-tests.cpp
@@ -2,7 +2,8 @@
 #include <string>
 #include <fstream>
 #include <iostream>
-#include "../graph.h"
+//#include "../graph.h"
+#include "../bfs.h"
 #include "../util/util.h"
 
 using namespace utils;
@@ -12,7 +13,7 @@ TEST_CASE("Parsed Graph Has Correct num_vertices", "[parser]") {
     std::string testing_vertex_list_path = "data_sample/nodes.txt";
     std::string testing_edge_list_path = "data_sample/edges.txt";
 
-    Graph g = utils::parse_to_graph(testing_vertex_list_path, testing_edge_list_path);
+    Graph* g = utils::parse_to_graph(testing_vertex_list_path, testing_edge_list_path);
     
     // count number of lines in vertex list
     int vertex_count = 0;
@@ -29,8 +30,8 @@ TEST_CASE("Parsed Graph Has Correct num_vertices", "[parser]") {
         return;
     }
     
-    REQUIRE( g.get_num_vertices() != 0 );
-    REQUIRE( g.get_num_vertices() == vertex_count );
+    REQUIRE( g->get_num_vertices() != 0 );
+    REQUIRE( g->get_num_vertices() == vertex_count );
 }
 
 /* NOTE: This test will need to be changed if the sample data files are edited */
@@ -39,12 +40,12 @@ TEST_CASE("Parsed Graph Has Correct Edges Manual", "[parser]") {
     std::string testing_vertex_list_path = "data_sample/nodes.txt";
     std::string testing_edge_list_path = "data_sample/edges.txt";
 
-    Graph g = utils::parse_to_graph(testing_vertex_list_path, testing_edge_list_path);
+    Graph* g = utils::parse_to_graph(testing_vertex_list_path, testing_edge_list_path);
 
-    REQUIRE( g.isAdjacent("0", "1") );
-    REQUIRE( g.isAdjacent("1", "2") );
-    REQUIRE( g.isAdjacent("2", "0") );
-    REQUIRE( g.isAdjacent("2", "1") );
+    REQUIRE( g->isAdjacent("0", "1") );
+    REQUIRE( g->isAdjacent("1", "2") );
+    REQUIRE( g->isAdjacent("2", "0") );
+    REQUIRE( g->isAdjacent("2", "1") );
 }
 
 /*
@@ -78,3 +79,48 @@ TEST_CASE("Parsed Graph Has Correct Edges Automatic", "[parser]") {
     REQUIRE( edge_count != 0 );
 }
 */
+
+// Note: this test case relies on the Graph and BFS constructors and centralitySorted()
+TEST_CASE("Centralities Save to File Correctly", "[centrality_saver]") {
+    std::string centrality_save_path = "sample_centralities";
+    std::string split_character = " ";
+
+    // set up graph values and expected results
+    std::vector<std::string> vertices = {"a", "b", "c"};
+    std::vector<std::pair<std::string, int>> ordered_centralities = {
+        {"b", 4},
+        {"a", 3},
+        {"c", 3},
+    };
+    std::vector<std::pair<std::string, std::string>> edges = {
+        {"a", "b"},
+        {"b", "c"},
+    };
+
+    // set up graph and save the centralities
+    Graph* g = new Graph(vertices, edges);
+    BFS bfs = BFS(g);
+    utils::save_centralities_to_file(centrality_save_path, bfs.centralitiesSorted());
+
+    // check if generated file has correct values in correct order
+    std::fstream centrality_file;
+    centrality_file.open(centrality_save_path, std::ios::in);
+
+    if (centrality_file.is_open()) {
+        std::string line;
+        int i = 0;
+
+        while (getline(centrality_file, line)) {
+            size_t split_pos = line.find(split_character);
+            std::pair<std::string, int> p = std::pair<std::string, int>(line.substr(0, split_pos),
+                                            stoi(line.substr(split_pos + split_character.length())));
+            REQUIRE( p == ordered_centralities[i]);
+            i++;
+        }
+
+        centrality_file.close();
+    } else {
+        std::cout << "File could not be opened! Please check your file path." << std::endl;
+        return;
+    }
+}
